5ce87e6a1a207d75df353e1a8f0b8a02d087ca72
Matt Shaw <m@ttshaw.com>
2021-12-20 09:15:30 -0500

    TaskAdmin

--- background_task/admin.py
+++ background_task/admin.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.utils import timezone
 from background_task.models import Task
 from background_task.models import CompletedTask
 
@@ -19,18 +18,11 @@ def dec_priority(modeladmin, request, queryset):
 dec_priority.short_description = 'priority -= 1'
 
 
-def run_now(modeladmin, request, queryset):
-    for task in queryset:
-        task.run_at = timezone.now()
-        task.save(update_fields=['run_at'])
-run_now.short_description = 'Run now'
-
-
 class TaskAdmin(admin.ModelAdmin):
     display_filter = ['task_name']
     search_fields = ['task_name', 'task_params', 'task_hash']
     list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
-    actions = [run_now, inc_priority, dec_priority]
+    actions = [inc_priority, dec_priority]
 
 
 class CompletedTaskAdmin(admin.ModelAdmin):
